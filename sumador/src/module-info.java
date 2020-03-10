c0b2991631bebda7fdb1851e990d337d1413f06b
Francisco Javier Riquelme <franriquelme17@gmail.com>
2020-03-10 14:00:22 +0100

    He creado la primera prueba y me da error

--- sumador/src/module-info.java
+++ sumador/src/module-info.java
@@ -1,2 +1,3 @@
 module sumador {
+	requires org.junit.jupiter.api;
 }
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -6,4 +6,19 @@ import org.junit.jupiter.api.Test;
 
 public class TestASumar {
 
+	/**
+	 * Si el resultado es = 7 sera válida
+	 */
+	
+	@Test
+	public void testUnaCifra() {
+		ASumar sumi = new ASumar ("7");
+		String resultado = sumi.mostrar();
+		assertEquals("7=7", resultado);
+	}
+
+	private void assertEquals(String string, String resultado) {
+		// TODO Auto-generated method stub
+		
+	}
 }
