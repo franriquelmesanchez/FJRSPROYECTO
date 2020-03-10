38aa4944f0da06d04404196f31fe8ded50fffb5b
Francisco Javier Riquelme <franriquelme17@gmail.com>
2020-03-10 13:44:11 +0100

    Creamos objeto de clase Asumar. Para esta prueba de un sólo dígito.

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -1,5 +1,32 @@
+/**
+ * 
+ */
 package sumador;
 
+/**
+ * @author Fran
+ *
+ */
 public class ASumar {
-
+	
+	private String valor_inicial;
+	
+	/**
+	 * @param string
+	 */
+	public ASumar (String string) {
+		this.valor_inicial= string;
+	}
+	
+	/**
+	 * @return devuelve un valor si es un número de un digito
+	 */
+	public String mostrar() {
+		String numero = valor_inicial;
+		if (numero.length()==1) {
+			return (numero + "=" + numero);
+		}
+			return null;
+	}
+	
 }
